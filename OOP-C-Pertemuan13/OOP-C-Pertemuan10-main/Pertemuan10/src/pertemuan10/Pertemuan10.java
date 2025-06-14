cbbbe46f732cf00fc6a3cfd9d507f914160721c5
ayenyehan <ayen19070@gmail.com>
2025-06-14 14:56:11 +0800

    Add files via upload

---
+++ OOP-C-Pertemuan13/OOP-C-Pertemuan10-main/Pertemuan10/src/pertemuan10/Pertemuan10.java
@@ -0,0 +1,17 @@
+package pertemuan10;
+
+/**
+ *
+ * @author Archangela Chirani Yehan
+ * TGL: 13 Juni 2025
+ */
+public class Pertemuan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
